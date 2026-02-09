4f6d8165e7008c0e5e88fbe597d3390ff2813f83
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2026-02-09 12:41:37 +0500

    ...

--- src/components/VerbCard.jsx
+++ src/components/VerbCard.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import VerbPronounsTable from "./Pronouns";
 
-
 export default function VerbCard({ verb }) {
     const [tense, setTense] = useState("present");
 
