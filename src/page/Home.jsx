6528a1eb429cf3cff69cff51ad8a6c6c10de7622
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2026-02-11 15:37:14 +0500

    book

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
-
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import bookPdf from "../assets/book/book.pdf";
 export default function Home() {
-    const { t } = useTranslation();
     return (
         <div>
-            <h2 className="mb-4">{t("home")}</h2>
+            <div style={{ width: "100%", height: "85vh", margin: "0 auto" }}>
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+                    <Viewer fileUrl={bookPdf} />
+                </Worker>
+            </div>
         </div>
     );
 }
